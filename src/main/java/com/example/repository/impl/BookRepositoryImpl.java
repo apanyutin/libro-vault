383b532114d8390f46bb01be26a889ad1fcd3dc7
Anton <panyutin15@gmail.com>
2024-08-03 17:03:49 +0300

    added implementation for BookRepositoryImpl and BookServiceImpl. Also added settings to application.properties

--- src/main/java/com/example/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/repository/impl/BookRepositoryImpl.java
@@ -3,17 +3,50 @@ package com.example.repository.impl;
 import com.example.model.Book;
 import com.example.repository.BookRepository;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Book save(Book book) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception exception) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save into DB the book: " + book, exception);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List findAll() {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Book", Book.class).getResultList();
+        } catch (Exception exception) {
+            throw new RuntimeException("Can't find all books in DB", exception);
+        }
+
     }
 }
