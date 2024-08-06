3835f99281d9a8f71480c8066d8643fd603acf0f
Anton <panyutin15@gmail.com>
2024-08-06 18:08:43 +0300

    added config, controller, dto, mapper for Book entity. Second task into in the same PR

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
