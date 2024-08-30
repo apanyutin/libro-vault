fdd7f909f231374aa5a60e5272f83459cfb794d0
Anton <panyutin15@gmail.com>
2024-08-30 21:59:11 +0300

    added Category entity

--- src/main/java/com/example/repository/book/BookRepository.java
+++ src/main/java/com/example/repository/book/BookRepository.java
@@ -1,9 +1,10 @@
 package com.example.repository.book;
 
 import com.example.model.Book;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-
+    List<Book> findAllByCategoryId(Long categoryId);
 }
